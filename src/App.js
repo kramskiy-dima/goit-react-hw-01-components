ec51784f87760e7c02fd4cec6dc74623b06cbbc2
Kramskiy Dima <64987701+ifinddima@users.noreply.github.com>
2021-02-21 18:37:52 +0200

    adds TransactionHistory component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Profile from "./components/Profile/Profile";
 import Statistics from "./components/Statistics/Statistics";
 import FriendListItem from "./components/FriendListItem/FriendListItem";
+import TransactionHistory from "./components/TransactionHistory/TransactionHistory";
 import user from "./bd/user.json";
 import staticalData from "./bd/statistical-data.json";
 import friends from "./bd/friends.json";
+import transactions from "./bd/transactions.json";
 
 function App() {
   return (
@@ -18,6 +20,7 @@ function App() {
       <Statistics title="Upload stats" stats={staticalData} />
       <Statistics stats={staticalData} />
       <FriendListItem props={friends} />
+      <TransactionHistory props={transactions} />
     </div>
   );
 }
